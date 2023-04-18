8981a79ac52ba9d622307ea9923ad8a53ec9dc78
Viktor Pikotkin <>
2023-04-18 19:22:27 +0300

    Add small QOF improvements, remove unnecessary packages

--- Assets/Scripts/Infrastructure/GameQuitManager.cs
+++ Assets/Scripts/Infrastructure/GameQuitManager.cs
@@ -1,18 +1,30 @@
 using Core.Data.Handlers;
 using UnityEngine;
 
+namespace Infrastructure
+{
 internal class GameQuitManager : MonoBehaviour
 {
+    [SerializeField] private KeyCode exitKeyBinding;
+    
     private GameDataManager _gameDataManager;
-
 
     public void Init(GameDataManager gameDataManager)
     {
         _gameDataManager = gameDataManager;
     }
-    
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(exitKeyBinding))
+        {
+            Application.Quit();
+        }
+    }
+
     private void OnApplicationQuit()
     {
         _gameDataManager.Save();
     }
+}
 }
